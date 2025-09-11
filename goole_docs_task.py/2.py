25bcc5112b2a49082530b49ab8cb7f44413c8fb3
Sarbuland <meharsahab435@gmail.com>
2025-09-12 02:18:20 +0500

    task match case

--- goole_docs_task.py/2.py
+++ goole_docs_task.py/2.py
@@ -1,22 +1,18 @@
-# def greeting(details):
-#     match details:
-#         case [time, name]:
-#             return f"Good {time} {name}"
-#         case [time, *names] if len(names) > 1:
-#           for i in names:
-#             print  (f"Good {time} {i}!")                     
-#         case _:
-#             return "Something went wrong"
+def greeting(details):
+    match details:
+        case [time, name]:
+            return f"Good {time} {name}"
+        case [time, *names] if len(names) > 1:
+          for i in names:
+            print  (f"Good {time} {i}!")                     
+        case _:
+            return "Something went wrong"
 
 
 
-# print(greeting(["Morning", "ali khan"]))
-# print(greeting(["Afternoon", "sara"]))
-# print(greeting(["Evening", "hassan", "bilal", "ahmad"]))
-
-
-
-
+print(greeting(["Morning", "ali khan"]))
+print(greeting(["Afternoon", "sara"]))
+print(greeting(["Evening", "hassan", "bilal", "ahmad"]))
 
 
 
@@ -26,9 +22,8 @@
 
 
 
-def my_func():
-    x = 10  # local variable
-    print("Inside function:", x)
 
-my_func()
-print(x)  #  Error: x is not defined
+
+
+
+
