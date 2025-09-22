d54d2a9da782b66e5ec816b2c308e2ec4debfc27
Sarbuland <meharsahab435@gmail.com>
2025-09-23 04:59:53 +0500

    practise

--- programin_task.py/Q1.py
+++ programin_task.py/Q1.py
@@ -1,21 +1,22 @@
 # 1. Write a program that takes a list of integers and removes all duplicates without using set()
 
-# lst=[1,2,3,3,3,4]
-# x=[]
-# for i in lst:
-#     if i not in x:
-#         x.append(i)
-#     else:
-#         x.remove(i)
-# print(x)
+lst=[1,2,3,3,3,4]
+x=[]
+for i in lst:
+    if i not in x:
+        x.append(i)
+    else:
+        x.remove(i)
+print(x)
 
+# R Work
+n=[1,2,2,3,4,4]
+y=0
+for i in n:
+    if n.count(i)==1:
+        y+=i
 
-n=[1,2,3,4,5]
-y="".join(map(str,n))
-rev=""
-for i in y:
-    rev=i+rev
-    r=list(rev)
+print(y)
     
 
     
